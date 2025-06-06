498dec8c7eba5258f542f88c1581b55f99455a4a
Prashant Yadav <146710834+prash08484@users.noreply.github.com>
2025-06-07 01:19:01 +0530

    Update Lazy-Segment-Tree.cpp

--- Segment-Fenwick-Tree/Lazy-Segment-Tree.cpp
+++ Segment-Fenwick-Tree/Lazy-Segment-Tree.cpp
@@ -1,108 +1,95 @@
-vi segTree; //  global segemnent tree 
-vi lazy; // lazy segment Tree
 
-
-int left(int i){
-    return 2*i; }
-int right(int i){
-    return 2*i+1;}
-
-/* NOTE : You can also take left as 2*i+1 and right as 2*i+2 
-           in place of index as 1 then you need to pass 0 */    
-
-// making segment tree : O(n)
-void build(vi &arr, int start ,int end ,int index){
-    if(start==end){
-    segTree[index]=arr[start];
-    return; }
-int mid=(start+end)/2; 
-build(arr,start,mid,left(index));
-build(arr,mid+1,end,right(index));
-segTree[index]=segTree[left(index)]+segTree[right(index)];
-}
-
+vll segTree,lazy;  // segment Tree,lazy Tree
+ 
+// helper function
+int left(int i){ return i<<1; }
+int right(int i){ return 1|(i<<1);}
+int treeSize(int n){ int sz=ceil(log2(n)); return 1<<(sz+1); }
 void propagate(int start,int end,int index){
-      if(start==end){
-        lazy[index]=0; return;  }
+     if(start==end){ lazy[index]=0; return; }
+     // UPDATE MAY NEEDED
+     lazy[left(index)]+=lazy[index];
+     lazy[right(index)]+=lazy[index];
+     lazy[i]=0; 
+}
+// UPDATE RETURN TYPE AND DATA TYPE IN PARAMETER
+ll merge(const ll &a,const ll &b){
+    return min(a,b);
+}
+
+// build segment tree 
+void build(auto &arr, int start ,int end ,int index){
+    if(start==end){ segTree[index]=arr[start]; return; }
+    int mid=start+(end-start)/2; 
+    build(arr,start,mid,left(index));
+    build(arr,mid+1,end,right(index));
+    segTree[index]=merge(segTree[left(index)],segTree[right(index)]);
+}
+
+// update [l,r] with value
+void update(int start,int end,int index,int l,int r,ll value){
     
-      lazy[2*index]+=lazy[index];
-      lazy[2*index+1]+=lazy[index];
-      lazy[index]=0;
-}
+    if(lazy[index]!=0){
+        segTree[index]+=lazy[index]; 
+        propagate(start,end,index); }
 
-// l,r is range to updated with value , index of segTree and start,end is range of segTree
-void update(vi &arr,int start,int end,int l,int r,int index,int value){
+    //disjoint 
+    if(l>end or r<start)return;
 
-    if(lazy[index]!=0){ // current have pending changes 
-      segTree[index]+=value;
-      propagate(start,end,index); } 
-  
-  if(r<start or l>end){ return; } // out of range 
-  
-  if(l>=start and r<=end){ // complete overlap 
-         segTree[index]+=value;
-         lazy[index]+=value;  
-         propagate(start,end,index);
-    return;
-  } 
-  
-// partial overlap 
-  int mid=(start+end)/2; 
-    update(arr,start,mid,l,r,left(index),value); 
-    update(arr,mid+1,end,l,r,right(index),value);
+    // complete overlap
+    if(start>=l and end<=r){
+        segTree[index]+=value;
+        lazy[index]+=value;
+        propagate(start,end,index); return;}
+
+    // partial overlap
+   int mid=start+(end-start)/2; 
+    update(start,mid,left(index),l,r,value);
+    update(mid+1,end,right(index),l,r,value);
    
-   segTree[index]=segTree[2*index]+segTree[2*index+1];
+   segTree[index]=merge(segTree[left(index)],segTree[right(index)]);
 }
 
+ // return [l,r]
+// UPDATE RETURN TYPE 
+ll query(int start,int end,int index,int l,int r){
 
-// to find sum from l to r 
-int query(int start,int end,int index,int l,int r){
-  
-     if(lazy[index]!=0){ // current have pending changes 
-      segTree[index]+=value;
-      propagate(start,end,index); } 
+     if(lazy[index]!=0){
+        segTree[index]+=lazy[index]; 
+        propagate(start,end,index); }
 
-if(l>end || r<start) /* curr sum is out of range */
-    return 0;
+// disjoint : UPDATE RETURN VALUE
+if(l>end or r<start){ return LINF; }
 
- /* curr sum is part of query e.g. ask for 1 to 4 
- : curr sum is 1 to 2 then it need so that here */ 
-if(start>=l&&end<=r)
-    return segTree[index];
+// complete overlap  
+if(start>=l and r>=end){
+ return segTree[index]; }
 
-int mid=(start+end)/2;
-int leftAns=query(start,mid,left(index),l,r);
-int rightAns=query(mid+1,end,right(index),l,r);
-return rightAns+leftAns;
+// partial overlap 
+int mid=start+(end-start)/2; 
+
+// UPDATE DATATYPE
+ll b=query(start,mid,left(index),l,r); 
+ll a=query(mid+1,end,right(index),l,r);
+return merge(a,b);  
 
 } 
 
-void Segment_Tree(){
+void solve(){
 
-int n; // size of given arr
-cin>>n;
+int n; // size of given array
+cin>>n;  
 
 // size req. to store segment tree
-int sz=ceil(log2(n));
-sz=2*pow(2,sz)-1;
-segTree.resize(sz,-1);
-lazy.resizse(sz,0);
+int sz=treeSize(n); 
 
-// taking array input 
-vi arr(n);
-read_vec(arr);
+segTree.resize(sz,0); 
+lazy.resize(sz,0); 
+ 
+vll arr(n);
+read(arr);
 
-// making segment tree
+// build segment tree
 build(arr,0,n-1,1);
-
-
-/* e.g. 
-
- debug(segTree);
- update(arr,0,n-1,1,2,-4);
- debug(query(0,n-1,1,3,3)); 
-
-*/
-
-
-}
+ 
+} 
