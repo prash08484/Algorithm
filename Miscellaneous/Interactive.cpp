3edd6866cfee281d66944f3b88dc62edbf0bbea5
Prashant Yadav <146710834+prash08484@users.noreply.github.com>
2025-08-26 12:57:07 +0530

    Document output flushing methods in Interactive.cpp

---
+++ Miscellaneous/Interactive.cpp
@@ -0,0 +1,14 @@
+// After printing each time either query or answer
+// print any one of below line ---
+
+// [ way 1 ]
+   
+cout << '\n';
+cout.flush();
+
+ // [ way 2 ]
+
+cout<<endl;
+
+// For more secure :
+// Turn of the sync function 
