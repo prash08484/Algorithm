30d0e90acc408aa45260bc38e03f92c30a4367c0
Prashant Yadav <146710834+prash08484@users.noreply.github.com>
2025-10-02 19:59:17 +0530

    Refactor Dijkstra function to use vector of vectors

--- Graph/Dijkstra.cpp
+++ Graph/Dijkstra.cpp
@@ -1,21 +1,22 @@
 // O(VlogV + ElogV)
 // Single Source Shortest Path
 
-void Dijkstra(int src, int n, vector<ll> &dist, vector<int> &parent, vector<pair<int, ll>> *adj) { 
+void Dijkstra(int src, int n, vector<ll> &dist, vector<int> &parent, vector<vector<pair<ll, ll>>> adj) { 
 	dist.assign(n, INF); // UPDATE NEEDED
 	parent.assign(n, -1);
 	dist[src] = 0;
 	priority_queue <pair<ll, ll>, vector<pair<ll, ll>>, greater<pair<ll, ll>>> q;
 	q.push({0, src});
 	while (!q.empty()) {
-		pair<ll, int> here = q.top();
+		pair<ll, ll> curr = q.top();
 		q.pop();
-		int u = here.second;
-		ll d_u = here.first;
-		if (d_u != dist[u])
+		int u = curr.second;
+		ll d_u = curr.first;
+		if (d_u != dist[u]){
 			continue;
-		for (auto it : adj[v]) {
-    int v=it.first, wt=it.second;
+	    }
+		for (auto it : adj[u]) {
+          int v=it.first, wt=it.second;
 			if (dist[u] + wt < dist[v]) {
 				dist[v] = dist[u] + wt;
 				parent[v] = u;
@@ -24,3 +25,4 @@ void Dijkstra(int src, int n, vector<ll> &dist, vector<int> &parent, vector<pair
 		}
 	}
 }
+
