0cd14c63ef3d2258c5571853878fb60d747c33f5
Prashant Yadav <146710834+prash08484@users.noreply.github.com>
2025-10-09 13:28:20 +0530

    Update trie.cpp

--- String/trie.cpp
+++ String/trie.cpp
@@ -1,27 +1,25 @@
-
-// Trie class
-class Trie {
-private:
-
-struct Node {  // Node structure for Trie
+ 
+ struct Node {  // Node structure for Trie
     Node* links[26]; 
     bool flag = false; 
-    bool avlKey(char ch) { return links[ch - 'a']!=NULL; }
+    bool has(char ch) { return links[ch - 'a']!=nullptr; }
     void put(char ch,Node* node){ links[ch - 'a'] = node; } 
     Node* get(char ch) { return links[ch - 'a']; } 
     void setEnd() { flag = true; } 
-    bool isEnd() { return flag;
-    }
+    bool isEnd() { return flag; }
 };
 
+// Trie class
+class Trie {
+ public:
+
     Node* root;
-public:
     Trie() { root = new Node(); }   
 
     void insert(string word) {
         Node* node = root;
         for (int i = 0; i<word.size(); i++) {
-            if (!node->avlKey(word[i])) 
+            if (!node->has(word[i])) 
                 node->put(word[i], new Node());  
             node = node->get(word[i]);
         } 
@@ -31,7 +29,7 @@ public:
     bool search(string word) {
         Node* node = root;
         for (int i = 0; i<word.size(); i++) {
-            if (!node->avlKey(word[i])) 
+            if (!node->has(word[i])) 
                 return false; 
             node = node->get(word[i]);
         } 
@@ -41,7 +39,7 @@ public:
     bool startsWith(string prefix) {
         Node* node = root;
         for (int i = 0; i<prefix.size(); i++) {
-            if (!node->avlKey(prefix[i])) 
+            if (!node->has(prefix[i])) 
                 return false; 
             node = node->get(prefix[i]);
         } 
