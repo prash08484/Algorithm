3378475f0040adf50bc0fd1894c00171a78cbf23
Prashant Yadav <146710834+prash08484@users.noreply.github.com>
2025-12-27 12:44:11 +0530

    Refactor Lazy Segment Tree implementation

--- Segment-Tree/Lazy_Seg_Tree.cpp
+++ Segment-Tree/Lazy_Seg_Tree.cpp
@@ -1,95 +1,115 @@
-
-vll segTree,lazy;  // segment Tree,lazy Tree
- 
-// helper function
-int left(int i){ return i<<1; }
-int right(int i){ return (i<<1)|1;}
-int treeSize(int n){ int sz=ceil(log2(n)); return 1<<(sz+1); }
-void propagate(int start,int end,int index){
-     if(start==end){ lazy[index]=0; return; }
-     // UPDATE MAY NEEDED
-     lazy[left(index)]+=lazy[index];
-     lazy[right(index)]+=lazy[index];
-     lazy[index]=0; 
-}
-// UPDATE RETURN TYPE AND DATA TYPE IN PARAMETER
-ll merge(const ll &a,const ll &b){
-    return min(a,b);
-}
-
-// build segment tree 
-void build(auto &arr, int start ,int end ,int index){
-    if(start==end){ segTree[index]=arr[start]; return; }
-    int mid=start+(end-start)/2; 
-    build(arr,start,mid,left(index));
-    build(arr,mid+1,end,right(index));
-    segTree[index]=merge(segTree[left(index)],segTree[right(index)]);
-}
-
-// update [l,r] with value
-void update(int start,int end,int index,int l,int r,ll value){
+class SegmentTree{
+public:
     
-    if(lazy[index]!=0){
-        segTree[index]+=lazy[index]; 
-        propagate(start,end,index); }
-
-    //disjoint 
-    if(l>end or r<start)return;
-
-    // complete overlap
-    if(start>=l and end<=r){
-        segTree[index]+=value; 
-        lazy[index]+=value;
-        propagate(start,end,index); return;}
-
-    // partial overlap
-   int mid=start+(end-start)/2; 
-    update(start,mid,left(index),l,r,value);
-    update(mid+1,end,right(index),l,r,value);
-   
-   segTree[index]=merge(segTree[left(index)],segTree[right(index)]);
-}
-
- // return [l,r]
-// UPDATE RETURN TYPE 
-ll query(int start,int end,int index,int l,int r){
-
-     if(lazy[index]!=0){
-        segTree[index]+=lazy[index]; 
-        propagate(start,end,index); }
-
-// disjoint : UPDATE RETURN VALUE
-if(l>end or r<start){ return LINF; }
-
-// complete overlap  
-if(start>=l and r>=end){
- return segTree[index]; }
-
-// partial overlap 
-int mid=start+(end-start)/2; 
-
-// UPDATE DATATYPE
-ll b=query(start,mid,left(index),l,r); 
-ll a=query(mid+1,end,right(index),l,r);
-return merge(a,b);  
-
-} 
-
-void solve(){
-
-int n; // size of given array
-cin>>n;  
-
-// size req. to store segment tree
-int sz=treeSize(n); 
-
-segTree.resize(sz,0); 
-lazy.resize(sz,0); 
+    vector<ll>segTree; // segment Tree
+    vector<ll>lazy;    // lazy Tree
+     
+    // Helper Function
+    int left(int i){ return i<<1; }
+    int right(int i){ return (i<<1)|1;}
+    int treeSize(int n){ int sz=ceil(log2(n)); return 1<<(sz+1); }
+    
+    void propagate(int start,int end,int index){ 
+        if(start==end){ 
+            lazy[index]=0; 
+            return; 
+        }
+        // UPDATE MAY NEEDED
+        lazy[left(index)]+=lazy[index];
+        lazy[right(index)]+=lazy[index]; 
+        lazy[index]=0; 
+    }
  
-vll arr(n);
-read(arr);
+    ll merge(const ll &a,const ll &b){
+        return a+b;
+    }
 
-// build segment tree
-build(arr,0,n-1,1);
- 
-} 
+    ll apply(const ll &a,const ll &b){
+        return a+b;
+    }
+
+    SegmentTree(vector<ll> &arr){
+        int n=arr.size(); 
+        
+        // size req. to store segment tree
+        int sz=treeSize(n); 
+
+        segTree.resize(sz,0); 
+        lazy.resize(sz,0); 
+        
+        // build segment tree
+        build(arr,0,n-1,1);
+    }
+
+    // build segment tree : O(n)
+    void build(vector<ll> &arr, int start, int end, int index){
+        
+        if(start==end){  
+            // UPDATE MAY NEEDED
+            segTree[index]=arr[start]; 
+            return; 
+        }
+
+        /* parital overlap */
+        int mid=start+(end-start)/2; 
+        build(arr,start,mid,left(index));
+        build(arr,mid+1,end,right(index));
+        
+        segTree[index]=merge(segTree[left(index)],segTree[right(index)]);
+    }
+    
+    /* UPDATE [l,r]  */
+    void update(int start,int end,int index,int l,int r,ll value){
+       
+        if(lazy[index]!=0){
+            segTree[index]=apply(segTree[index],lazy[index]); 
+            propagate(start,end,index); 
+        }
+    
+        /* out of range */ 
+        if(l>end or r<start)return;
+    
+        /* completer overlap */
+        if(start>=l and end<=r){
+            segTree[index]=apply(segTree[index],value); 
+            lazy[index]+=value; 
+            propagate(start,end,index); 
+            return;
+        }
+    
+        /* parital overlap */
+        int mid=start+(end-start)/2; 
+        update(start,mid,left(index),l,r,value);
+        update(mid+1,end,right(index),l,r,value);
+       
+       segTree[index]=merge(segTree[left(index)],segTree[right(index)]);
+    }
+
+    /* QUERY [l,r]  */
+    ll query(int start,int end,int index,int l,int r){
+    
+        if(lazy[index]!=0){
+            segTree[index]=apply(segTree[index],lazy[index]); 
+            propagate(start,end,index); 
+        }
+    
+        /* out of range */
+        if(l>end or r<start){ 
+            // UPDATE MAY NEEDED
+            return 0; 
+        }
+        
+        /* completer overlap */
+        if(start>=l and r>=end){
+            return segTree[index]; 
+        }
+        
+        /* parital overlap */
+        int mid=start+(end-start)/2; 
+        
+        // UPDATE DATATYPE
+        ll b=query(start,mid,left(index),l,r); 
+        ll a=query(mid+1,end,right(index),l,r);
+        return merge(a,b);  
+    } 
+};
