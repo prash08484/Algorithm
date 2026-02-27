eaa0ebc2f41c1dee829e0d5cd4bf908b0e45182f
Prashant Yadav <146710834+prash08484@users.noreply.github.com>
2026-02-27 23:38:34 +0530

    Booth algo
    Lexo smallest roatate array

---
+++ String/Booth.cpp
@@ -0,0 +1,33 @@
+
+// Lexo smallest roatate array where T.C : O(n) 
+vector<ll> lexo(vector<ll>& a) {
+    int n = a.size();
+    if (n<=1) return a;
+
+    vector<ll> b(2*n);
+    for (int i=0; i<2*n; i++) b[i] = a[i%n];
+
+    int i=0, j=1, k=0;
+    while (i<n && j<n && k<n) {
+        if (b[i+k] == b[j+k]) {
+            k++;
+        } 
+        else if (b[i+k] < b[j+k]) {
+            j = j+k+1;
+            if (i==j) j++;
+            k=0;
+        } 
+        else {
+            i = i+k+1;
+            if (i == j) i++;
+            k = 0;
+        }
+    }
+
+    int start = min(i,j);
+    vector<ll> res(n);
+    for (int i=0; i<n; i++){ 
+        res[i] = a[(start+i)%n];
+    }
+    return res;
+}
