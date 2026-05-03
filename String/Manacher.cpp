e513ee90e10e3b7fe6bb089cc62f05a2c30b1e7b
Prashant Yadav <146710834+prash08484@users.noreply.github.com>
2026-05-03 14:15:00 +0530

    Refactor Manacher's algorithm for palindrome detection

--- String/Manacher.cpp
+++ String/Manacher.cpp
@@ -1,32 +1,37 @@
 // longest Palindromic Substring in O(n)
-vector<int>manacher_odd(string &s){
+vector<int>manacher(string &str){
 
-int n=s.size();
-s="$" +s+ "^"; // to ensure end of string 
+  //  string transform for odd length handling
+  //  insert # between each char
+
+  string s=""; 
+  for(char &it:str)
+    s+=string("#")+it; 
+  s+="#";
+
+  int n=s.size();
+  s="$" +s+ "^"; // to ensure end of string 
+   
+  vector<int>p(n+2,0); // radius of palindrome,center at i
+  int l=0,r=0;
+  
+  for(int i=1;i<=n;i++){
+    int j=l+r-i; // mirror pos of i wrt [l,r]
+  
+    p[i]=max(0,min(r-i,p[j]));
+
+    while(s[i-p[i]]==s[i+p[i]])
+      p[i]++; 
+  
+    // update [l,r]
+    if(i+p[i]>r){
+      l=i-p[i];
+      r=i+p[i]; 
+    }
+  }
+
+  return p;
+
+}
+
  
-vector<int>p(n+2); 
-int l=0,r=1;
-
-for(int i=1;i<=n;i++){
-  int j=l+r-i; // mirror pos of i wrt [l,r]
-
-  p[i]=min(r-i,p[j]);
-  while(s[i-p[i]]==s[i+p[i]]){
-    p[i]++; }
-
-// update [l,r]
-  if(i+p[i]>r){
-    l=i-p[i],r=i+p[i]; }
-}
-
-return vector<int>(begin(p)+1,end(p)-1); 
-
-}
-
-vector<int> manacher(string &s){
-    string t=""; // make odd string 
-    for(char &it:s){
-        t+=string("#")+it; }
-    vector<int>p=manacher_odd(t+"#");
-    return vector<int>(begin(p)+1,end(p)-1); 
-}
