6cb49568dddd9306b0f1ea697635a5c14505136a
Prashant Yadav <146710834+prash08484@users.noreply.github.com>
2026-05-18 13:12:08 +0530

    Improve Manacher's algorithm implementation
    Refactor Manacher's algorithm implementation for clarity and efficiency.

--- String/Manacher.cpp
+++ String/Manacher.cpp
@@ -1,37 +1,40 @@
-// longest Palindromic Substring in O(n)
-vector<int>manacher(string &str){
+// Longest Palindromic Substring in O(n)
+vector<int> manacher(string& str) {
 
-  //  string transform for odd length handling
-  //  insert # between each char
+    // transform string
+    string s = "";
+    for (char &it : str)
+        s += "#" + string(1, it);
+    s += "#";
 
-  string s=""; 
-  for(char &it:str)
-    s+=string("#")+it; 
-  s+="#";
-
-  int n=s.size();
-  s="$" +s+ "^"; // to ensure end of string 
-   
-  vector<int>p(n+2,0); // radius of palindrome,center at i
-  int l=0,r=0;
+    int n = s.size();
   
-  for(int i=1;i<=n;i++){
-    int j=l+r-i; // mirror pos of i wrt [l,r]
-  
-    p[i]=max(0,min(r-i,p[j]));
+    // sentinels
+    s = "$" + s + "^";
 
-    while(s[i-p[i]]==s[i+p[i]])
-      p[i]++; 
-  
-    // update [l,r]
-    if(i+p[i]>r){
-      l=i-p[i];
-      r=i+p[i]; 
+    vector<int> p(n + 2, 0);
+
+    int l = 0, r = 0;
+
+    for (int i = 1; i <= n; i++) {
+
+        int j = l + r - i; // mirror index
+
+        if (i < r) 
+          p[i] = min(r - i, p[j]);
+
+        // expand
+        while (s[i - (p[i] + 1)] == s[i + (p[i] + 1)])
+            p[i]++;
+
+        // update best segment
+        if (i + p[i] > r) {
+            l = i - p[i];
+            r = i + p[i];
+        }
     }
-  }
-
+  
+    str=s;
+    
   return p;
-
 }
-
- 
