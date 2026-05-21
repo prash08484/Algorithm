2e87cfbf0359cf34ffb1cb1f452d9c6ab9b83801
Prashant Yadav <146710834+prash08484@users.noreply.github.com>
2026-05-21 18:30:06 +0530

    Refactor articulation point algorithm with DFS

--- Graph/ArticulationPoint.cpp
+++ Graph/ArticulationPoint.cpp
@@ -3,26 +3,60 @@
 // ans : articulation points
 // disc,low : discovery time and low value 
 
-int timer=0; 
-void articulation(auto &adj,auto &vis,auto &ans,auto &disc,auto &low,int node,int parent){
-    vis[node]=1;
-    disc[node]=low[node]=timer++;
-    int child=0; 
-    for(auto &it:adj[node]){
-        if(it==parent)continue; 
-        if(!vis[it]){
-            articulation(adj,vis,ans,disc,low,it,node);
-            low[node]=min(low[node],low[it]); 
-            if(low[it]>=disc[node] and parent!=-1){
-                ans.push_back(node);
+vector<int> articulationPoints(int n, vector<vector<int>>& eg) { 
+
+    vector<vector<int>>adj(n);  
+     
+    for(auto &it:eg){
+        int u=it[0],v=it[1];  
+        adj[u].emplace_back(v);
+        adj[v].emplace_back(u);
+    } 
+
+    vector<bool>vis(n,0); 
+    vector<bool>ans(n,0);
+    vector<int>tin(n); // time to enter node
+    vector<int>low(n); // lowest possible time to reach any node in descendent
+    int timer=0;         
+    
+    function<void(int,int)>dfs=[&](int i,int p)->void{
+        timer++; 
+        tin[i]=low[i]=timer;
+        vis[i]=1;
+        int child=0;
+        
+        for(auto &it:adj[i]){
+            if(it==p)continue; 
+            if(vis[it]){
+                low[i]=min(low[i],tin[it]); // Back-edge !! NOTE : NOT take low[it] 
             }
-            child++;
-        }
-        else{
-            low[node]=min(low[node],disc[it]); 
-        }
+            else{
+                dfs(it,i); 
+                child++;
+                
+                low[i]=min(low[i],low[it]); 
+                
+                if(low[it]>=tin[i] and p!=-1){ // if no descendent node have better reachibility than parent
+                    ans[i]=1;
+                }
+            }
+        } 
+        
+    if(child>1 and p==-1){ // for root node child>1 then it's AP
+        ans[i]=1;
     }
-    if(child>1 and parent==-1){
-        ans.push_back(node);
+            
+    };
+        
+    for(int i=0;i<n;i++){
+        if(!vis[i])dfs(i,-1); 
     }
+      
+        
+    vector<int>res; 
+    for(int i=0;i<n;i++){
+        if(ans[i])res.emplace_back(i);
+    }
+         
+    return res; 
 }
