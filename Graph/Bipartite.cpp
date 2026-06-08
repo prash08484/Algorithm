892a1e78c2c18b17b0f27e1157050cb7d5c79d93
Prashant Yadav <146710834+prash08484@users.noreply.github.com>
2026-06-08 12:06:04 +0530

    Refactor Bipartite graph functions for clarity

--- Graph/Bipartite.cpp
+++ Graph/Bipartite.cpp
@@ -1,32 +1,27 @@
-
 // T.C. : O(n+m)
 
-bool dfsBipartite(vector<int>&color,vector<vector<int>>&adj,int c,int i){
+void dfsBipartite(vector<int>&color, vector<vector<int>>&adj, int c, int i, bool &ok){
     color[i]=c;
-    for(auto &it:adj[i]){
-        if(color[it]==0){
-            bool chk=dfsBipartite(color,adj,3-c,it);
-            if(!chk)return false ; }
-        else if(color[it]==c){
-            return false;
+    for(auto &it : adj[i]){
+        if(color[it] == -1){
+            dfsBipartite(color, adj, 3^c, it, ok); 
+        } 
+        else if(color[it] == c){
+            ok=0;
         }
-    }
-    return true;
+    } 
 } 
 
-bool Bipartite(int n,auto &adj,vector<int>&color){ 
-for(int i=1;i<=n;i++){
-    if(color[i]==0 && !dfsBipartite(color,adj,1,i){
-        // IMPOSSIBLE
-             return 0;
-    }
-} 
-  return 1;  
-  
+bool Bipartite(int n, vector<int>&color, vector<vector<int>> &adj){ 
+
+    fill(begin(color),end(color),-1);
+    
+    bool ok = 1; 
+    for(int i=0;i<n;i++){
+        if(color[i] != -1)continue; 
+        dfsBipartite(color, adj, 0, i, ok);
+    } 
+    return ok;  
 }
  
  
- 
- 
-
-
