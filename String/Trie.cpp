0a3ac8ffe5c142afee6968c2822c9eac751494c7
Prashant Yadav <146710834+prash08484@users.noreply.github.com>
2026-08-22 20:38:10 +0530

    Refactor Trie class and add search methods
    Refactored Trie class and Node structure for better initialization and added new methods for word and prefix searches.

--- String/Trie.cpp
+++ String/Trie.cpp
@@ -1,49 +1,55 @@
+
+class Trie{
+
+    struct Node{
+        Node* next[26];
+        bool flag;
+        Node(){
+            flag=0;
+            for(int i=0; i<26; i++)
+                next[i]=nullptr;
+        }
+    }; 
+
+    public:
+    
+    Node* root; 
+
+    Trie(){
+        root=new Node();
+    }
+
+    // insert a new word
+    void insertWord(string &word){
+        Node* tRoot=root; 
+        for(char c: word){
+            if(tRoot->next[c-'a']==nullptr){
+                tRoot->next[c-'a']=new Node();
+            }
+            tRoot=tRoot->next[c-'a'];
+        }
+        tRoot->flag=1;
+    }
+
+    // serach a word
+    bool searchWord(string &word){
+        Node* tRoot=root; 
+        for(char c: word){
+            if(tRoot->next[c-'a']==nullptr) return 0;
+            tRoot=tRoot->next[c-'a'];
+        }
+        return tRoot->flag; 
+    }
+    
+    // prefix search
+    bool prefixSearch(string &word){
+        Node* tRoot=root;
+        for(char c: word){
+            if(tRoot->next[c-'a']==nullptr) return 0;
+            tRoot=tRoot->next[c-'a'];
+        }
+        return 1; 
+    }
+
+}; 
  
- struct Node {  // Node structure for Trie
-    Node* next[26]; 
-    bool flag = false; 
-    bool has(char ch) { return next[ch - 'a']!=nullptr; }
-    void put(char ch,Node* node){ next[ch - 'a'] = node; } 
-    Node* get(char ch) { return next[ch - 'a']; } 
-    void setEnd() { flag = true; } 
-    bool isEnd() { return flag; }
-};
-
-// Trie class
-class Trie {
- public:
-
-    Node* root;
-    Trie() { root = new Node(); }   
-
-    void insert(string word) {
-        Node* node = root;
-        for (int i = 0; i<word.size(); i++) {
-            if (!node->has(word[i])) 
-                node->put(word[i], new Node());  
-            node = node->get(word[i]);
-        } 
-        node->setEnd();
-    }
-
-    bool search(string word) {
-        Node* node = root;
-        for (int i = 0; i<word.size(); i++) {
-            if (!node->has(word[i])) 
-                return false; 
-            node = node->get(word[i]);
-        } 
-        return node->isEnd();
-    }
- 
-    bool startsWith(string prefix) {
-        Node* node = root;
-        for (int i = 0; i<prefix.size(); i++) {
-            if (!node->has(prefix[i])) 
-                return false; 
-            node = node->get(prefix[i]);
-        } 
-        return true; 
-    }
-
-};
